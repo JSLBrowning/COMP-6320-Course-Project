a74e6e6fa9bce713cf4d323127e29cf6f9437bd6
JAMES BROWNING <james@wallofhistory.com>
2022-11-27 14:41:25 -0600

    Update main.c

--- main.c
+++ main.c
@@ -17,15 +17,20 @@ int main() {
 }
 
 
-int random_queue_selection_system(int lambda, int mu) {
+int randomQueueSelectionSystem(int lambda, int mu) {
+   // Since this is just a simulation, queues can just be ints.
+   int queue1 = 0;
+   int queue2 = 0;
+
    // Packets arrive following a Poisson process of rate lambda.
    // Packets are assigned to one of the two queues at random.
+
    // If the queue is full (one packet being processed and nine in waiting), the new packet is dropped.
    // Packets are processed at an exponentially distributed rate with mean 1/mu.
 }
 
 
-int min_queue_selection_system(int lambda, int mu) {
+int minQueueSelectionSystem(int lambda, int mu) {
    // Packets arrive following a Poisson process of rate lambda.
    // Packets are assigned to whichever queue has the fewest packets in waiting.
       // Random queue if both are the same length.
