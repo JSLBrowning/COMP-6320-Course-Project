d829c2f8378d2cd8e8a598c5a63e4e794d22d1e2
JAMES BROWNING <james@wallofhistory.com>
2022-11-28 17:49:01 -0600

    Update main.c

--- main.c
+++ main.c
@@ -8,9 +8,10 @@
  * AUTHORS:
  *       James S. L. Browning and Austin David Preston
  *H*/
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-RAND_MAX = 32767;
 
 
 double randomNumber() {
@@ -45,48 +46,55 @@ int randomQueueSelectionSystem(int lambda, int mu) {
 
 
    while( packetsArrived < 10000 && queue1 > 0 && queue2 > 0 ) {
-      if (arrivalCountdown == 0) {
-         // Generate a random number with a Poisson distribution,
-         // where lambda is the mean of the distribution.
-         arrivalCountdown = randomPoisson(lambda);
+      if (arrivalCountdown == 0 && packetsArrived < 10000) {
+         // Send the new arrival to the queues.
          if (queue1 < 10 && queue2 < 10) {
             // If both queues are less than 10, randomly select one.
             if (randomNumber() < 0.5) {
                queue1++;
+               packetsArrived++;
             } else {
                queue2++;
+               packetsArrived++;
             }
-         } else if (queue1 < 10 && queue2 >= 10) {
+         } else if (queue1 < 10 && queue2 == 10) {
             queue1++;
             packetsArrived++;
-         } else if (queue2 < 10 && queue1 >= 10) {
+         } else if (queue2 < 10 && queue1 == 10) {
             queue2++;
             packetsArrived++;
          } else {
             droppedPackets++;
             packetsArrived++;
          }
+
+         // Regenerate the arrival countdown.
+         arrivalCountdown = randomPoisson(lambda);
       }
+
       if (queueOneDepartureCountdown == 0) {
          if (queue1 > 0) {
             queue1--;
             queueOneDepartureCountdown = randomPoisson(mu);
          }
       }
+
       if (queueTwoDepartureCountdown == 0) {
          if (queue2 > 0) {
             queue2--;
             queueTwoDepartureCountdown = randomPoisson(mu);
          }
       }
+
       arrivalCountdown--;
       queueOneDepartureCountdown--;
       queueTwoDepartureCountdown--;
    }
 
-   printf("DROPPED PACKETS: %d", droppedPackets);
+   printf("DROPPED PACKETS: %d\n", droppedPackets);
    int successfulPackets = packetsArrived - droppedPackets;
-   printf("PACKETS ARRIVED: %d", successfulPackets);
+   printf("PACKETS ARRIVED: %d\n", successfulPackets);
+   return successfulPackets;
 }
 
 
@@ -102,66 +110,87 @@ int minQueueSelectionSystem(int lambda, int mu) {
 
 
    while( packetsArrived < 10000 && queue1 > 0 && queue2 > 0 ) {
-      if (arrivalCountdown == 0) {
-         // Generate a random number with a Poisson distribution,
-         // where lambda is the mean of the distribution.
-         arrivalCountdown = randomPoisson(lambda);
+      if (arrivalCountdown == 0 && packetsArrived < 10000) {
+         // Send the new arrival to the queues.
          if (queue1 < 10 && queue2 < 10) {
-            // If both queues are less than 10, put it in the shortest queue.
             if (queue1 < queue2) {
                queue1++;
+               packetsArrived++;
             } else if (queue2 < queue1) {
                queue2++;
+               packetsArrived++;
             } else {
                // If the queues are equal, randomly select one.
                if (randomNumber() < 0.5) {
                   queue1++;
+                  packetsArrived++;
                } else {
                   queue2++;
+                  packetsArrived++;
                }
             }
-         } else if (queue1 < 10 && queue2 >= 10) {
+         } else if (queue1 < 10 && queue2 == 10) {
             queue1++;
             packetsArrived++;
-         } else if (queue2 < 10 && queue1 >= 10) {
+         } else if (queue2 < 10 && queue1 == 10) {
             queue2++;
             packetsArrived++;
          } else {
             droppedPackets++;
             packetsArrived++;
          }
+
+         // Regenerate the arrival countdown.
+         arrivalCountdown = randomPoisson(lambda);
       }
+
       if (queueOneDepartureCountdown == 0) {
          if (queue1 > 0) {
             queue1--;
             queueOneDepartureCountdown = randomPoisson(mu);
          }
       }
+
       if (queueTwoDepartureCountdown == 0) {
          if (queue2 > 0) {
             queue2--;
             queueTwoDepartureCountdown = randomPoisson(mu);
          }
       }
+
       arrivalCountdown--;
       queueOneDepartureCountdown--;
       queueTwoDepartureCountdown--;
    }
 
-   printf("DROPPED PACKETS: %d", droppedPackets);
+   printf("DROPPED PACKETS: %d\n", droppedPackets);
    int successfulPackets = packetsArrived - droppedPackets;
-   printf("PACKETS ARRIVED: %d", successfulPackets);
+   printf("PACKETS ARRIVED: %d\n", successfulPackets);
+   return successfulPackets;
 }
 
 
-int main(int choice, int lambda, int mu) {
-   if(choice == 1) {
-      randomQueueSelectionSystem(lambda, mu);
-   } else if(choice == 2) {
-      minQueueSelectionSystem(lambda, mu);
-   } else {
+int main(int argc, char *argv[]) {
+   if (argc != 4) {
       printf("Invalid parameters.\n");
       printf("Usage: ./main <choice> <lambda> <mu>\n");
       printf("Choice: 1 for random queue selection, 2 for minimum queue selection.\n");
+      return 1;
+   } else {
+      int choice = atoi(argv[1]);
+      int lambda = atoi(argv[2]);
+      int mu = atoi(argv[3]);
+
+      if (choice == 1) {
+         randomQueueSelectionSystem(lambda, mu);
+      } else if (choice == 2) {
+         minQueueSelectionSystem(lambda, mu);
+      } else {
+         printf("Invalid parameters.\n");
+         printf("Usage: ./main <choice> <lambda> <mu>\n");
+         printf("Choice: 1 for random queue selection, 2 for minimum queue selection.\n");
+      }
+
+      return 0;
    }
 }
